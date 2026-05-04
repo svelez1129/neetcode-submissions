a4f68262a274413f5fccc7f909f930a420fe65f5
Sergio Velez <108548203+svelez1129@users.noreply.github.com>
2026-05-04 01:17:37 -0400

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.py
@@ -0,0 +1,30 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        #check how many zeroes there are, if more than two zeroes everything is just zero
+        zeroRet = [0 for i in range(n)]
+        zeroCnt = 0
+        for num in nums:
+            if num == 0:
+                zeroCnt += 1
+            if zeroCnt > 1:
+                return zeroRet 
+       #do prefix mult
+        prefix = [nums[i] for i in range(n)]
+        for i in range(1,n):
+            prefix[i] *= prefix[i-1]
+        #do suffix mult
+        suffix = [nums[i] for i in range(n)]
+        for j in range(n-2,0,-1):
+            suffix[j] *= suffix[j+1]
+        ret = []
+        for i in range(n):
+            if zeroCnt == 1 and nums[i] != 0:
+                ret.append(0)
+            elif i == 0:
+                ret.append(suffix[i+1])
+            elif i == n-1:
+                ret.append(prefix[i-1])
+            else:
+                ret.append(prefix[i-1]*suffix[i+1])
+        return ret
